f0ecceb407e8557c5536066ce21f61ffb990bd0b
DmitryBurnaev <dmitry.burnaev@gmail.com>
2021-10-23 21:52:06 +0300

    [#1] Currency Exchange providers integration

--- src/providers.py
+++ src/providers.py
@@ -1,4 +1,7 @@
+import json
 import os
+
+from urllib.request import urlopen
 
 
 class ExchangeInfoProvider:
@@ -7,4 +10,18 @@ class ExchangeInfoProvider:
 
     def get_rate(self):
         url = f'{self.source_url}?access_key={self.source_api_key}&format=1&base=EUR&symbols=RUB'
-        return {}
+        print(url)
+        try:
+            with urlopen(url) as resp:
+                response_text = resp.read().decode()
+                response_data = json.loads(response_text)
+        except Exception as err:
+            print(err)
+
+        print(response_data)
+        return response_data
+
+
+if __name__ == '__main__':
+    pr = ExchangeInfoProvider()
+    pr.get_rate()
