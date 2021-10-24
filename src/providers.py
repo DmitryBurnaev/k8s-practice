420d80b33a22c4dd6d53e8570327ef5dba77e211
DmitryBurnaev <dmitry.burnaev@gmail.com>
2021-10-24 16:25:01 +0300

    [#1] Updated logic for currency provider

--- src/providers.py
+++ src/providers.py
@@ -1,27 +1,43 @@
 import json
 import os
+import logging
+from typing import Optional
 
+from urllib.parse import urlencode
 from urllib.request import urlopen
+
+
+logger = logging.getLogger(__name__)
 
 
 class ExchangeInfoProvider:
     source_url = 'http://data.fixer.io/api/latest'
     source_api_key = os.getenv('FIXER_API_KEY')
 
-    def get_rate(self):
-        url = f'{self.source_url}?access_key={self.source_api_key}&format=1&base=EUR&symbols=RUB'
-        print(url)
+    def get_rate(self, base: str = 'EUR', target: str = 'RUB') -> Optional[float]:
+        query_string = urlencode({
+            'format': 1,
+            'base': base,
+            'symbols': target,
+            'access_key': self.source_api_key,
+        })
         try:
-            with urlopen(url) as resp:
+            with urlopen(f'{self.source_url}?{query_string}') as resp:
                 response_text = resp.read().decode()
                 response_data = json.loads(response_text)
-        except Exception as err:
-            print(err)
 
-        print(response_data)
-        return response_data
+            rate = response_data["rates"][target]
+
+        except KeyError as err:
+            logger.error("Couldn't parse currency rate response: key not found (%s) | resp: %s", err, response_data)
+
+        except Exception as err:
+            logger.exception("Couldn't get currency exchange rate: %s", err)
+
+        else:
+            return rate
 
 
 if __name__ == '__main__':
     pr = ExchangeInfoProvider()
-    pr.get_rate()
+    print(pr.get_rate())
--- src/server.py
+++ src/server.py
@@ -5,6 +5,8 @@ import sys
 from http import HTTPStatus
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 
+from src.providers import ExchangeInfoProvider
+
 logger = logging.getLogger(__name__)
 
 
@@ -34,29 +36,46 @@ class CurrencyExchangeServer(ThreadingHTTPServer):
 
 class CurrencyExchangeHTTPHandler(BaseHTTPRequestHandler):
 
+    class GetRateException(Exception):
+        def __init__(self, message: str):
+            self.message = message
+
+        def __str__(self):
+            return self.message
+
     def do_GET(self):
-        content = json.dumps(self._get_exchange())
-        self.send_response(HTTPStatus.OK)
+        try:
+            currency_rate_content = self._get_exchange_rate()
+        except self.GetRateException as err:
+            content = json.dumps({"status": "ERROR", "message": err.message})
+            self.send_response(HTTPStatus.SERVICE_UNAVAILABLE)
+        else:
+            content = json.dumps({"status": "OK", "rate": currency_rate_content})
+            self.send_response(HTTPStatus.OK)
+
         self.send_header("Content-type", "application/json")
         self.send_header("Content-Length", str(len(content)))
         self.send_header("Last-Modified", self.date_time_string())
         self.end_headers()
         self.wfile.write(content.encode())
 
-    @staticmethod
-    def _get_exchange(base_currency='USD'):
-        logger.info('Getting exchange rate: base currency %s', base_currency)
-        return {'GBP': 1.4}
+    def _get_exchange_rate(self, base_currency='EUR', target_currency='RUB'):
+        logger.info("Getting exchange rate: base currency %s", base_currency)
+        pr = ExchangeInfoProvider()
+        if rate := pr.get_rate(base=base_currency, target=target_currency):
+            return {base_currency: rate}
+
+        raise self.GetRateException("Couldn't get currency rate...")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--bind', '-b', metavar='ADDRESS',
+    parser.add_argument("--bind", "-b", metavar="ADDRESS",
                         help='Specify alternate bind address [default: all interfaces]')
-    parser.add_argument('port', action='store', default=8000, type=int,
-                        nargs='?', help='Specify alternate port [default: 8000]')
+    parser.add_argument("port", action="store", default=8000, type=int,
+                        nargs="?", help="Specify alternate port [default: 8000]")
     args = parser.parse_args()
 
     with CurrencyExchangeServer(args.bind, args.port) as httpd:
